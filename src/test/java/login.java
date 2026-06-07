1027bef54312da160108b7d23a3dda9d567c0b63
Sandhiya Devi <sandhiyadevi2927@gmal.com>
2026-06-07 22:14:21 +0530

    updated testng.xml for jenkins

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -5,49 +5,51 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class login {
     WebDriver driver;
-    public static void main(String[] args) {
+    @FindBy(id="user-name")
+    WebElement user;
+    @FindBy (id="password")
+    WebElement pwd;
+    @FindBy (id="login-button")
+    WebElement loginBtn;
 
+    @BeforeMethod
+    public void setup() {
         ChromeOptions options = new ChromeOptions();
-      Map<String, Object> prefs = new HashMap<>();
+        Map<String, Object> prefs = new HashMap<>();
         prefs.put("credentials_enable_service", false);
         prefs.put("profile.password_manager_enabled", false);
         prefs.put("profile.password_manager_leak_detection", false);
         options.setExperimentalOption("prefs", prefs);
-        WebDriver driver = new ChromeDriver(options);
-        //WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
-        login log = new login(driver);
-        log.userLogin();
-        /*
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-        */    }
-
-    @FindBy(id="user-name")
-        WebElement user;
-    @FindBy (id="password")
-    WebElement pwd;
-    @FindBy (id="login-button")
-    WebElement login;
-    public login(WebDriver driver)
-    {
-        this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
+
+   @Test
     public void userLogin()
     {
         user.sendKeys("standard_user");
         pwd.sendKeys("secret_sauce");
-        login.click();
+        loginBtn.click();
+        System.out.println("Title: " + driver.getTitle());
+        System.out.println("URL: " + driver.getCurrentUrl());
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {   if(driver!=null) {
+        driver.quit();
+    }
     }
 
 }
